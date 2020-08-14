0217eac50e26140906893d77b69f47c0d1556b18
BrendonLH <Brendonhampton1988@gmail.com>
2020-08-13 20:49:51 -0700

    changes made on Thu Aug 13 20:49:51 PDT 2020

--- __tests__/notes.test.js
+++ __tests__/notes.test.js
@@ -2,10 +2,7 @@
 
 const Request = require('../lib/notes');
 
-describe('request', () => {
-    it('requests command', (obj) => {
-        expect(this.command).toBe(obj);
-        expect(this.command instanceof Request).toBeTruthy();
-    })
-})
+
+
+
 
---
+++ github.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+git add .
+DATE=$(date)
+git commit -m "changes made on $DATE"
+git push origin $1
+#END
