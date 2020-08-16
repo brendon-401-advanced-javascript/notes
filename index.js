9036f511bebff45b10e79b56a488419a3d0c18ce
BrendonLH <Brendonhampton1988@gmail.com>
2020-08-15 18:54:55 -0700

    trying to refactor my code and get it linked up to my DB

--- index.js
+++ index.js
@@ -2,6 +2,18 @@
 // require dependancies and connect files
 const Input = require('./lib/input.js');
 const Request = require('./lib/notes.js');
+const mongoose = require('mongoose');
+const notesDB = require('./lib/model/notes-schema.js');
+
+// // connect to mongo
+const MONGODB_URI = 'mongodb://localhost:27017/notesey';
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+// insert record
+
+// query the DB
 
 // take input
 let input = new Input();
@@ -9,7 +21,9 @@ let input = new Input();
 const request = new Request(input);
 
 // push output
-const output = request.go();
-console.log(output);
+console.log(input.command);
+const outputAdd = request;
+
+console.log(outputAdd);
 
 
