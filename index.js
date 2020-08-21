9b0364fa959c057cc76e5d0cb2878f85d2289bce
BrendonLH <Brendonhampton1988@gmail.com>
2020-08-20 22:22:35 -0700

    got add and list to work properly with help from TA

--- index.js
+++ index.js
@@ -2,15 +2,10 @@
 // require dependancies and connect files
 const Input = require('./lib/input.js');
 const Request = require('./lib/notes.js');
-const mongoose = require('mongoose');
-const Entry = require('./lib/model/notes-schema.js');
+
 
 // // connect to mongo
-const MONGODB_URI = 'mongodb://localhost:27017/notesey';
-mongoose.connect(MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+
 
 
 
@@ -18,40 +13,19 @@ mongoose.connect(MONGODB_URI, {
 // take input
 let input = new Input();
 const request = new Request(input);
+request.execute();
 
 // handle the newNotes with Database
-let newNotes = {
-    name: request.name,
-    note: request.note,
-    category: request.category
-}
+
 // console.log(request.note);
 
-async function addANote() {
-    try {
-    let note = new Entry(newNotes);
-    let addedNote = await note.save();
-    console.log(`Note added: ${request.add()} `);
-    console.log(`${addedNote}`)
 
-   
-    } catch(e) {
-        console.error(e);
-    }
-}
 
-async function listNotes() {
-    try {
-    let allNotes = await Entry.find({});
-    console.log('all notes', allNotes);
-    } catch(e) {
-        console.error(e);
-    }
-}
+
 
 // addANote();
-
-listNotes();
+// console.log(input);
+// listNotes();
 
 
 
